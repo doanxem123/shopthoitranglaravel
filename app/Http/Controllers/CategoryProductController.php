47b3a63fb1986180a02de6d12c260aca20a5f1a6
pro9x3105 <55211825+pro9x3105@users.noreply.github.com>
2020-10-25 22:26:59 +0700

    25_10_2020

---
+++ app/Http/Controllers/CategoryProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CategoryProductController extends Controller
+{
+	//Thêm danh mục sản phẩm
+    public function add_category_product(){
+    	return View();
+    }
+
+    //Hiển thị toàn bộ danh mục sản phẩm
+    public function all_category_product(){
+    	return View();
+    }
+}
